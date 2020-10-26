e61023ca493ffa81540c2b3b238c93e70d18b5d4
Dror Meidan <meidan.dror@gmail.com>
2020-10-26 14:52:44 +0200

    level1.1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace BasicProject
 {
     class Program
     {
-        static void Main(string[] args)
+        public void SubmitInvitation() 
         {
             Console.WriteLine("Welcome to Hamburger resturant");
             List<Hamburger> hamburgers = new List<Hamburger>();
@@ -16,15 +16,15 @@ namespace BasicProject
             List<int> sextras = new List<int>();
             int sbread = new int();
             int contE = new int();
-            
-            while(Convert.ToBoolean(contD)) 
+
+            while (Convert.ToBoolean(contD))
             {
                 Console.WriteLine("choose meat: 0-meat, 1-lamb, 2-tofo");
                 smeat = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("choose bread: 0- brown, 1-white");
                 sbread = Convert.ToInt32(Console.ReadLine());
-                contE= 1;
-                while(Convert.ToBoolean(contE)) 
+                contE = 1;
+                while (Convert.ToBoolean(contE))
                 {
                     Console.WriteLine("choose extra: 0-cucamber, 1-tomato, 2-lettuce, 3-pickels, 4-onion");
                     sextras.Add(Convert.ToInt32(Console.ReadLine()));
@@ -40,13 +40,19 @@ namespace BasicProject
 
             int Tprice = new int();
             Tprice = 0;
-            foreach (Hamburger item in hamburgers) 
+            foreach (Hamburger item in hamburgers)
             {
-                 Tprice += item.price();
+                Tprice += item.price();
             }
 
             Console.WriteLine("The total price");
             Console.WriteLine(Tprice);
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.SubmitInvitation();
         }
     }
 }
